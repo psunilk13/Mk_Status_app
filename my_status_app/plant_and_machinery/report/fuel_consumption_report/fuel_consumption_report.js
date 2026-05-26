7bdee7fec182c0e312133fdc1efe378960397c7c
psunilk13 <121981835+psunilk13@users.noreply.github.com>
2026-05-26 20:00:09 +0530

    Update fuel_consumption_report.js

--- my_status_app/plant_and_machinery/report/fuel_consumption_report/fuel_consumption_report.js
+++ my_status_app/plant_and_machinery/report/fuel_consumption_report/fuel_consumption_report.js
@@ -19,12 +19,12 @@ frappe.query_reports["Fuel Consumption Report"] = {
             fieldtype: "Date",
             reqd: 0
         },
-		{
-            fieldname: "asset_category",
-            label: "Asset Category",
-            fieldtype: "Link",
-            options: "Asset Category"
-        },
+		// {
+  //           fieldname: "asset_category",
+  //           label: "Asset Category",
+  //           fieldtype: "Link",
+  //           options: "Asset Category"
+  //       },
 		
         {
             fieldname: "asset",
