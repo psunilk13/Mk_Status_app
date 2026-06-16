5d6e874ca0c8d18984da84c620379f595db585d2
Vamsi-28062003 <vamsisivvala2806@gmail.com>
2026-06-16 16:16:59 +0530

    Add maintenance category filter with dynamic options
    Added a new filter for maintenance category with dynamic options based on the selected category.

--- my_status_app/plant_and_machinery/report/maintenance_report/maintenance_report.js
+++ my_status_app/plant_and_machinery/report/maintenance_report/maintenance_report.js
@@ -2,23 +2,17 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Maintenance Report"] = {
-
     filters: [
-
         {
             fieldname: "from_date",
             label: "From Date",
-            fieldtype: "Date",
-            reqd: 0
+            fieldtype: "Date"
         },
-
         {
             fieldname: "to_date",
             label: "To Date",
-            fieldtype: "Date",
-            reqd: 0
+            fieldtype: "Date"
         },
-
         {
             fieldname: "asset",
             label: "Asset",
@@ -31,7 +25,56 @@ frappe.query_reports["Maintenance Report"] = {
             fieldtype: "Link",
             options: "Asset Category"
         },
+        {
+            fieldname: "maintenance_category",
+            label: "Maintenance Category",
+            fieldtype: "Select",
+            options: "\nPeriodical\nPredictive\nBreakdown",
 
+            on_change: function () {
+
+                let category = frappe.query_report.get_filter_value(
+                    "maintenance_category"
+                );
+
+                let options = "";
+
+                switch (category) {
+
+                    case "Periodical":
+                        options = "\nService\nLubricants";
+                        break;
+
+                    case "Predictive":
+                        options = "\nSpares";
+                        break;
+
+                    case "Breakdown":
+                        options = "\nBreakdown";
+                        break;
+
+                    default:
+                        options = "\nService\nSpares\nLubricants\nBreakdown";
+                }
+
+                let maintenance_type =
+                    frappe.query_report.get_filter(
+                        "maintenance_type"
+                    );
+
+                maintenance_type.df.options = options;
+                maintenance_type.set_input("");
+                maintenance_type.refresh();
+
+                frappe.query_report.refresh();
+            }
+        },
+        {
+            fieldname: "maintenance_type",
+            label: "Maintenance Type",
+            fieldtype: "Select",
+            options: "\nService\nSpares\nLubricants\nBreakdown"
+        },
         {
             fieldname: "item_code",
             label: "Item Code",
