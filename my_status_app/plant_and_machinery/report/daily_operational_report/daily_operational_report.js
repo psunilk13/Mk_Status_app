0ddcf0c5fba10aac073dac89d5d8874a59a64ab1
Vamsi-28062003 <vamsisivvala2806@gmail.com>
2026-07-28 14:28:29 +0530

    Refactor daily operational report JavaScript file
    Removed redundant comments and cleaned up the code structure.

--- my_status_app/plant_and_machinery/report/daily_operational_report/daily_operational_report.js
+++ my_status_app/plant_and_machinery/report/daily_operational_report/daily_operational_report.js
@@ -1,3 +1,41 @@
+// // // Copyright (c) 2026, shiva and contributors
+// // // For license information, please see license.txt
+
+// // frappe.query_reports["Daily Operational Report"] = {
+
+// //     filters: [
+
+// //         {
+// //             fieldname: "from_date",
+// //             label: "From Date",
+// //             fieldtype: "Date",
+// //             reqd: 0
+// //         },
+
+// //         {
+// //             fieldname: "to_date",
+// //             label: "To Date",
+// //             fieldtype: "Date",
+// //             reqd: 0
+// //         },
+
+// //         {
+// //             fieldname: "asset",
+// //             label: "Asset",
+// //             fieldtype: "Link",
+// //             options: "Asset"
+// //         },
+// //         {
+// //             fieldname: "asset_category",
+// //             label: "Asset Category",
+// //             fieldtype: "Link",
+// //             options: "Asset Category"
+// //         }
+// //     ]
+// // };
+// // Copyright (c) 2026, shiva and contributors
+// // For license information, please see license.txt
+
 // // Copyright (c) 2026, shiva and contributors
 // // For license information, please see license.txt
 
@@ -8,15 +46,12 @@
 //         {
 //             fieldname: "from_date",
 //             label: "From Date",
-//             fieldtype: "Date",
-//             reqd: 0
+//             fieldtype: "Date"
 //         },
-
 //         {
 //             fieldname: "to_date",
 //             label: "To Date",
-//             fieldtype: "Date",
-//             reqd: 0
+//             fieldtype: "Date"
 //         },
 
 //         {
@@ -25,6 +60,7 @@
 //             fieldtype: "Link",
 //             options: "Asset"
 //         },
+
 //         {
 //             fieldname: "asset_category",
 //             label: "Asset Category",
@@ -33,40 +69,4 @@
 //         }
 //     ]
 // };
-// Copyright (c) 2026, shiva and contributors
-// For license information, please see license.txt
-
-// Copyright (c) 2026, shiva and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Daily Operational Report"] = {
-
-    filters: [
-
-        {
-            fieldname: "from_date",
-            label: "From Date",
-            fieldtype: "Date"
-        },
-        {
-            fieldname: "to_date",
-            label: "To Date",
-            fieldtype: "Date"
-        },
-
-        {
-            fieldname: "asset",
-            label: "Asset",
-            fieldtype: "Link",
-            options: "Asset"
-        },
-
-        {
-            fieldname: "asset_category",
-            label: "Asset Category",
-            fieldtype: "Link",
-            options: "Asset Category"
-        }
-    ]
-};
 
